a988dc837aabd12d1d0714dd6c24474d7f56d7e6
sergio.a.matteoda@gmail.com <Rafaela1604!>
2024-10-08 08:42:50 -0300

    Constants update

--- NetStandard/SDK/turboSMTP.Test/AppConstants.cs
+++ NetStandard/SDK/turboSMTP.Test/AppConstants.cs
@@ -4,17 +4,17 @@ namespace turboSMTP.Test
 {
     internal static class AppConstants
     {
-        public static readonly string ConsumerKey = "1ef0eef86f089b18cb610532beecf72e";
-        public static readonly string ConsumerSecret = "GIx7Z0OncXf9oANe8z3gUQ6wYPtJH21d";
+        public static readonly string ConsumerKey = "ConsumerKey";
+        public static readonly string ConsumerSecret = "ConsumerSecret";
 
         public static readonly string ServerUrl = "https://pro.api.serversmtp.com/api/v2";
         public static readonly string SendServerUrl = "https://api.turbo-smtp.com/api/v2";
 
-        public static readonly string EmailSender = "msaad@emailchef.com";
+        public static readonly string EmailSender = "emailsender";
         public static readonly List<string> ValidEmailAddresses = new List<string>()
         {
-            "sergioamatteoda@gmail.com",
-            "sergio.test2@turbo-smtp.com"
+            "live@service.alibaba.com",
+            "live@service.alibaba.com"
         };
         public static readonly List<string> InvalidEmailAddresses = new List<string>()
         {
